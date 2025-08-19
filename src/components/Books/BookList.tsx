465c17418ea2eef1f49133121ed29d97ee71627e
tafhimulazwad-ctrl <tafhimulazwad@gmail.com>
2025-08-19 14:01:29 +0600

    Fix BookCard import statement

---
+++ src/components/Books/BookList.tsx
@@ -0,0 +1,6 @@
+@@ .. @@
+ import { useBooks } from '../../context/BookContext';
+ import { useAuth } from '../../context/AuthContext';
+-import BookCard from './BookCard';
++import { BookCard } from './BookCard';
+ import BookDetail from './BookDetail';
